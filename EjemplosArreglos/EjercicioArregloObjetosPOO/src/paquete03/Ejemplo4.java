b37e952af01fc905cecacce6684cc449bf7f04ac
fabian <frcanar2@utpl.edu.ec>
2024-05-16 15:25:48 -0500

    clase05

--- EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
+++ EjemplosArreglos/EjercicioArregloObjetosPOO/src/paquete03/Ejemplo4.java
@@ -24,7 +24,7 @@ public class Ejemplo4 {
         
         for (int i = 0; i < calificaciones.length; i++) {
             Calificacion objetoCalificacion = calificaciones[i];
-            System.out.printf("%s - %.2f\n - Profesor(%s)\n", 
+            System.out.printf("%s - %.2f - Profesor(%s)\n", 
                     objetoCalificacion.obtenerNombreMateria(),
                     objetoCalificacion.obtenerNota(),
                     // Me sirve para porder presentar valores de otras clases 
